4939e665923c2f268f38ba5d78d7b5281a25a573
fenajain <81793724+fenajain@users.noreply.github.com>
2023-05-29 17:56:25 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -14,7 +14,7 @@ N = st.text_input("Enter the Nitrogen Content (1-150) ")
 if N:
     try:
         Nitrogen = float(N)
-        if Nitrogen < 1 or Nitrogen > 150 :
+        if Nitrogen < 1 or Nitrogen > 150:
             st.error("Nitrogen content should be between 1 to 150.")
         else:
             input_data.append(Nitrogen)
@@ -73,11 +73,11 @@ if hum:
 ph = st.text_input("Enter the pH Content (3-10) ")
 if ph:
     try:
-       pH = float(ph)
-       if pH < 3 or pH > 10:
-           st.error("pH content should be between 3 to 10.")
-       else:
-           input_data.append(pH)
+        pH = float(ph)
+        if pH < 3 or pH > 10:
+            st.error("pH content should be between 3 to 10.")
+        else:
+            input_data.append(pH)
 
     except ValueError:
         st.error("Invalid Input, Please Enter a Numeric Value.")
@@ -96,10 +96,12 @@ if rf:
 
 result = ""
 
+
 def crop_prediction(ip_data):
     input_array = np.asarray(ip_data)
     reshaped_data = input_array.reshape(1, -1)
-    num_data = reshaped_data.astype(np.float64)        # The value received from above is in string form so to convert that into string we did this
+    num_data = reshaped_data.astype(
+        np.float64)  # The value received from above is in string form so to convert that into string we did this
     return num_data
 
 
@@ -108,51 +110,51 @@ try:
         final = crop_prediction(input_data)
         result = model.predict(final)
         res = np.array(result)
-        st.write("<span style = 'font-size : 24px'> The output is <u><b>{}</b></u></span>".format(res[0].capitalize()),unsafe_allow_html = True)
+        st.write("<span style = 'font-size : 24px'> The output is <u><b>{}</b></u></span>".format(res[0].capitalize()),
+                 unsafe_allow_html=True)
 
         crop_name = res[0]
         seasonal_crop = suggest_season(crop_name, df)
-        st.write("<span style = 'font-size : 24px'> Your crop {} is <u><b>{}</b></u> crop</span>".format(res[0],seasonal_crop.capitalize()),unsafe_allow_html = True)
+        st.write("<span style = 'font-size : 24px'> Your crop {} is <u><b>{}</b></u> crop</span>".format(res[0],
+                                                                                                         seasonal_crop.capitalize()),
+                 unsafe_allow_html=True)
 
         stats = summary(crop_name)
         st.write(stats)
 
-        
         data_frame = summary(crop_name)
-        st.markdown("<h1 style='text-align: left; font-size: 30px;'>Different Quantity of Chemical Content in {} crop</h1>".format(res[0].capitalize()), unsafe_allow_html=True)
-        fig, ax = plt.subplots(figsize=(8,4))
-        sns.lineplot(x="Labels", y="Minimum", data=data_frame, ax=ax , color = "red" , label = "Minimum Content")
-        sns.lineplot(x="Labels", y="Average", data=data_frame, ax=ax , color = "blue" , label = "Average Content")
-        sns.lineplot(x="Labels", y="Maximum", data=data_frame, ax=ax, color="green" , label = "Maximum Content")
+        st.markdown(
+            "<h1 style='text-align: left; font-size: 30px;'>Different Quantity of Chemical Content in {} crop</h1>".format(
+                res[0].capitalize()), unsafe_allow_html=True)
+        fig, ax = plt.subplots(figsize=(8, 4))
+        sns.lineplot(x="Labels", y="Minimum", data=data_frame, ax=ax, color="red", label="Minimum Content")
+        sns.lineplot(x="Labels", y="Average", data=data_frame, ax=ax, color="blue", label="Average Content")
+        sns.lineplot(x="Labels", y="Maximum", data=data_frame, ax=ax, color="green", label="Maximum Content")
         # Display plot on Streamlit
         ax.legend(loc='upper left')
-        ax.set_xlabel("Chemical Components in the Soil",fontsize=10)
+        ax.set_xlabel("Chemical Components in the Soil", fontsize=10)
         ax.tick_params(axis='x', labelsize=8)
-        ax.set_ylabel('Quantity of Chemical Components',fontsize = 10)
+        ax.set_ylabel('Quantity of Chemical Components', fontsize=10)
         ax.tick_params(axis='y', labelsize=8)
         st.pyplot(fig)
 
-except Exception as e:
-    st.error(f"Error occurred is: {e}")
 
-try:
-
-    if st.button("Data_Analytics"):
+    # if st.button("Data_Analytics"):
         frame = pd.read_csv("detailed_cropdata.csv")
 
-        st.markdown("<h1 style='text-align: left; font-size: 30px;'>Overall Data Visualization </h1>", unsafe_allow_html=True)
-        fig, ax = plt.subplots(figsize=(5,2))
+        st.markdown("<h1 style='text-align: left; font-size: 30px;'>Overall Data Visualization </h1>",
+                    unsafe_allow_html=True)
+        fig, ax = plt.subplots(figsize=(5, 2))
         # plt.figure(figsize=(5,2))
         ax.bar(frame['Crop_Labels'], frame['N-Avg'], color='thistle')
         plt.xticks(rotation=90)
         # Set plot title and axis labels
-        ax.set_title('Different Crops VS Average Nitrogen Content',fontsize = 10)
+        ax.set_title('Different Crops VS Average Nitrogen Content', fontsize=10)
         ax.set_xlabel('Different Crops', fontsize=8)
         ax.tick_params(axis='x', labelsize=7)
         ax.set_ylabel('Average Nitrogen Content', fontsize=8)
         ax.tick_params(axis='x', labelsize=7)
         st.pyplot(fig)
-
 
         fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 4))
         ax1.bar(frame['Crop_Labels'], frame['P-Avg'], color='khaki')
@@ -160,7 +162,7 @@ try:
         ax1.set_xlabel('Different Crops', fontsize=10)
         ax1.tick_params(axis='x', labelsize=7)
         ax1.set_ylabel('Average Phosphorus Content', fontsize=10)
-        ax1.tick_params(axis='x', labelsize=7 , rotation = 90)
+        ax1.tick_params(axis='x', labelsize=7, rotation=90)
 
         sns.kdeplot(frame['P-Avg'], shade=True, color='slateblue', ax=ax2)
         ax2.set_title('Distribution of Average Phosphorus Content', fontsize=12)
@@ -169,7 +171,6 @@ try:
         ax2.set_ylabel('Density', fontsize=10)
         ax2.tick_params(axis='x', labelsize=7)
         st.pyplot(fig)
-
 
         fig, ax = plt.subplots(figsize=(5, 2))
         # Sort the data
@@ -184,7 +185,6 @@ try:
         ax.tick_params(axis='x', labelsize=7)
         st.pyplot(fig)
 
-
         fig, ax = plt.subplots(figsize=(8, 4))
         sns.lineplot(x="Crop_Labels", y="T-Avg", data=frame, ax=ax, color="hotpink", label="Avg. Temp")
         sns.lineplot(x="Crop_Labels", y="H-Avg", data=frame, ax=ax, color="cornflowerblue", label="Avg. Humidity")
@@ -196,7 +196,6 @@ try:
         ax.set_ylabel('Quantity of Chemical Components', fontsize=10)
         ax.tick_params(axis='y', labelsize=8)
         st.pyplot(fig)
-
 
         fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 4))
         ax1.bar(frame['Crop_Labels'], frame['ph-Avg'], color="burlywood")
@@ -214,22 +213,19 @@ try:
         ax2.tick_params(axis='x', labelsize=7)
         st.pyplot(fig)
 
-except Exception as e:
-    st.error(f"Error occurred is: {e}")
 
-try:
-    st.markdown("<h1 style='text-align: left; font-size: 24px;'>Some Interesting Facts to known about crops</h1>",unsafe_allow_html = True)
-    if st.button("Facts") :
-
+    st.markdown("<h1 style='text-align: left; font-size: 24px;'>Some Interesting Facts to known about crops</h1>",
+                unsafe_allow_html=True)
+    if st.button("Facts"):
         output = knowing_facts(df)
 
         st.write("1. {} requires very high ratio of Nitrogen content in soil.".format(output[0].capitalize()))
         st.write("2. {} and {} requires very high ratio of Phosphorus content in soil.".format(output[1].capitalize(),output[2].capitalize()))
-        st.write("3. {} and {} requires very high ratio of Potassium content in soil.".format(output[3].capitalize(), output[4].capitalize()))
+        st.write("3. {} and {} requires very high ratio of Potassium content in soil.".format(output[3].capitalize(),output[4].capitalize()))
         st.write("4. {}, {} and {} requires very high ratio of Rainfall.".format(output[5].capitalize(), output[6].capitalize(), output[7].capitalize()))
         st.write("5. {} requires very low Temperature content.".format(output[8].capitalize()))
         st.write("6. {} requires very high Temperature content in soil.".format(output[10].capitalize()))
-        st.write("7. {} and {} requires very low ratio of Humidity content in soil.".format(output[11].capitalize(), output[12].capitalize()))
+        st.write("7. {} and {} requires very low ratio of Humidity content in soil.".format(output[11].capitalize(),output[12].capitalize()))
         st.write("8. {} requires very both high and low pH content ranging between 3.5 to 10".format(output[13].capitalize()))
 
 except Exception as e:
